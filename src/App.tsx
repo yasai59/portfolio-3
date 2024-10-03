f24f2b950b7fedfc5c3012580a95704e59c7481a
Yasai <alexvidalcasado@gmail.com>
2024-10-03 14:02:20 +0200

    [INIT]

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header"
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <div className="container">
+        <h1 className="font-title text-[7rem] font-extrabold">Web developer <br/> & Student</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { LanguageSelector } from "./LanguageSelector";
+
+export const Header = () => {
+  return <div className="h-28 flex items-center w-full">
+    <div className="container text-xl flex justify-between">
+      <h2>Alejandro <br /> Vidal Casado</h2>
+      <ul className="flex gap-10 items-center">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <LanguageSelector/>
+    </div>
+  </div>;
+};
